26f818126652c0fd736f0a8ef44984bd458e805f
Krystian994 <63874373+Krystian994@users.noreply.github.com>
2025-04-18 16:16:21 +0200

    update

--- Ajax/public/js/produkty.js
+++ Ajax/public/js/produkty.js
@@ -1,30 +1,34 @@
 $(document).ready(function() {
 
+    // Kliknięcia w linki paginacji
     $(document).on('click', '.pagination a', function(event) {
         event.preventDefault();
         var url = $(this).attr('href');
         loadProducts(url);
     });
 
-    $(document).on('submit', '#search-form', function(event) {
-        event.preventDefault();
-        var url = $(this).attr('action') + '?' + $(this).serialize();
-        loadProducts(url);
-    });
-
-    $(document).on('click', 'a.btn-dark, a.btn-primary, a.btn-success', function(event) {
+    // Kliknięcie przycisków kategorii
+    $(document).on('click', '.btn-primary, .btn-dark, .btn-success', function(event) {
         event.preventDefault();
         var url = $(this).attr('href');
         loadProducts(url);
     });
 
+    // Wysyłanie formularza wyszukiwania
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var url = $(this).attr('action') + '?' + $(this).serialize();
+        loadProducts(url);
+    });
+
+    // Funkcja AJAX do ładowania produktów
     function loadProducts(url) {
         $.ajax({
             url: url,
             type: 'GET',
             success: function(data) {
                 $('#produkty-wrapper').html(data);
-                window.history.pushState({}, '', url);
+                window.history.pushState({}, '', url); // zmiana adresu URL bez przeładowania strony
             },
             error: function() {
                 alert('Wystąpił błąd podczas ładowania produktów.');
